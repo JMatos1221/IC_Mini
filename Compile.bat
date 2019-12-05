97e70eef40455e0c576f05fc3322986463e722ce
Joao Matos <joao.matos1221@gmail.com>
2019-12-05 11:20:00 +0000

    First Working Commit

---
+++ Compile.bat
@@ -0,0 +1,3 @@
+gcc -c -g -Wall -Wpedantic -Wextra -std=c99 sortify.c -o sortify.o
+gcc sortify.o -o sortify
+pause
---
+++ sortify.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MSG_WELCOME "WELCOME TO SORTIFY!"
+#define MSG_SORT "Sort the following numbers:"
+#define MSG_SORT2 "Please sort the numbers"
+#define MSG_WELL "Well done!"
+#define MSG_WIN "Congratulations, you win!"
+#define MSG_OVER "Game Over."
+#define MSG_WRONG "Wrong answer."
+#define MSG_MAX "You have reached the maximum number of moves."
+#define MSG_BYE "Bye."
+#define MSG_UNKNOWN "Unknown option."
+/* Use puts() to print constant strings */
+
+int rand_number(const int, const int);
+void print_status(const int, const int, const int);
+void print_menu(void);
+
+
+int main(int argc, char **argv)
+{
+	char command;
+	int gameOver = 0, gameLevel = 1, gameScore = 0, gamePlays = 0;
+
+
+	puts(MSG_WELCOME);
+	print_menu();
+
+	do{scanf("%c", &command);
+
+	switch (command)
+	{
+	case 'p':
+		break;
+
+	case 'q':
+		gameOver = 1;
+		break;
+
+	case 'm':
+		print_menu();
+		break;
+
+	case 's':
+		print_status(gameLevel, gameScore, gamePlays);
+		break;
+	
+	default:
+		break;
+	}
+} while (gameOver == 0);
+	return 0;
+}
+
+/* generate a random integer between min and max */
+int rand_number(const int min, const int max)
+{
+	if (max < min)
+	{
+		puts("Max must be larger than Min");
+		exit(0);
+	}
+	int n = abs(max - min) + 1;
+	return (rand() % n) + min;
+}
+
+/* print the game status */
+void print_status(const int level, const int score, const int plays)
+{
+	puts("+-----------------------------+");
+	printf("| level:  %02d                  |\n", level);
+	printf("| points: %02d                  |\n", score);
+	printf("| plays:  %02d                  |\n", plays);
+	puts("+-----------------------------+");
+}
+
+/* print the option menu */
+void print_menu(void)
+{
+	puts("+-----------------------------+");
+	puts("| SORTIFY                     |");
+	puts("| p - next chalenge           |");
+	puts("| q - quit                    |");
+	puts("| m - print this information  |");
+	puts("| s - show your status        |");
+	puts("+-----------------------------+");
+}
